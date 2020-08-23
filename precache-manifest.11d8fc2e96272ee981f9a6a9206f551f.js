bb0e84d8568053020cfbbc5880adb75b39bb8566
Annie <annievampirerox@gmail.com>
2020-08-23 16:04:38 -0400

    app

---
+++ precache-manifest.11d8fc2e96272ee981f9a6a9206f551f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1865102d6ff13bee483ec9db568c34",
+    "url": "/IgnitionHacks/index.html"
+  },
+  {
+    "revision": "a22858e53214bdac5d76",
+    "url": "/IgnitionHacks/static/css/2.f21ee910.chunk.css"
+  },
+  {
+    "revision": "b5d89415afd1fd1da870",
+    "url": "/IgnitionHacks/static/css/main.389f04da.chunk.css"
+  },
+  {
+    "revision": "a22858e53214bdac5d76",
+    "url": "/IgnitionHacks/static/js/2.99631b63.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/IgnitionHacks/static/js/2.99631b63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5d89415afd1fd1da870",
+    "url": "/IgnitionHacks/static/js/main.5c5c755e.chunk.js"
+  },
+  {
+    "revision": "8e846609ea94ae7d20d8",
+    "url": "/IgnitionHacks/static/js/runtime-main.4a1ec979.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/IgnitionHacks/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/IgnitionHacks/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/IgnitionHacks/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/IgnitionHacks/static/media/slick.f97e3bbf.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/IgnitionHacks/precache-manifest.11d8fc2e96272ee981f9a6a9206f551f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/IgnitionHacks/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
